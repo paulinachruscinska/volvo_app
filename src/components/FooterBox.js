bb253b7cf13426268a1f6bd5d25bd87a895bc63e
Your Name <you@example.com>
2022-12-04 19:36:57 +0100

    Add mobile Main section

---
+++ src/components/FooterBox.js
@@ -0,0 +1,28 @@
+import {useState} from "react";
+import classNames from "classnames";
+
+ export default function FooterBox({item, index}){
+    const [active, setActive] = useState('')
+    const [arrow, setArrow] = useState('')
+    return(
+        <div className='footer__box' key={index} >
+            <p  onClick={()=>{
+                return (
+                    active=== '' ? setActive(item.li) : setActive(''),
+                        arrow === '' ? setArrow(item.ul) : setArrow('')
+                )
+            }} className={classNames('footer__box--text',{
+                changeArrow: arrow === item.ul
+            })}>{item.ul}</p>
+            <ul className={classNames('footer__box--list ', 'footer__box--list' + index, {
+                clicked: active === item.li
+            })}>
+                {item.li.map((list, index)=>{
+                    return (
+                        <li className={'list list' + index} key={index}>{list}</li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
+}
