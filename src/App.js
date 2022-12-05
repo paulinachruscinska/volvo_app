5aa0e83206b8997e59d2471caa055835171d1cef
Your Name <you@example.com>
2022-12-05 19:50:01 +0100

    Add navigation

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Introduction from "./components/Introduction";
 import MoreAbout from "./components/MoreAbout";
 import Questions from "./components/Questions";
 import Footer from "./components/Footer";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
       <>
+          <Navigation/>
           <Main/>
           <Introduction/>
           <MoreAbout/>
